43eb7da8a6b0c739ab478e8f86d3ef381cebed8b
Aaron Murray <plugins@omv-extras.org>
2014-10-28 09:13:05 -0500

    initial commit

---
+++ var/www/openmediavault/js/omv/module/admin/service/developer/Plugins.js
@@ -0,0 +1,226 @@
+/**
+ * @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+ * @author    Volker Theile <volker.theile@openmediavault.org>
+ * @author    OpenMediaVault Plugin Developers <plugins@omv-extras.org>
+ * @copyright Copyright (c) 2009-2013 Volker Theile
+ * @copyright Copyright (c) 2014 OpenMediaVault Plugin Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+// require("js/omv/WorkspaceManager.js")
+// require("js/omv/workspace/grid/Panel.js")
+// require("js/omv/workspace/window/Form.js")
+// require("js/omv/workspace/window/plugin/ConfigObject.js")
+// require("js/omv/util/Format.js")
+// require("js/omv/Rpc.js")
+// require("js/omv/data/Store.js")
+// require("js/omv/data/Model.js")
+// require("js/omv/data/proxy/Rpc.js")
+// require("js/omv/form/field/SharedFolderComboBox.js")
+
+Ext.define("OMV.module.admin.service.wol.Plugins", {
+    extend   : "OMV.workspace.grid.Panel",
+    requires : [
+        "OMV.Rpc",
+        "OMV.data.Store",
+        "OMV.data.Model",
+        "OMV.data.proxy.Rpc",
+        "OMV.util.Format"
+    ],
+
+    hidePagingToolbar : false,
+    hideAddButton     : true,
+    hideEditButton    : true,
+    hideDeleteButton  : true,
+    autoReload        : false,
+    stateful          : true,
+    stateId           : "b317a72d-1804-1632-a31b-1f48f0ea6aae",
+    columns           : [{
+        text      : _("Name"),
+        sortable  : true,
+        dataIndex : "name",
+        stateId   : "name"
+    },{
+        text      : _("Full Name"),
+        sortable  : true,
+        dataIndex : "fullname",
+        stateId   : "fullname"
+    },{
+        text      : _("Github URL"),
+        sortable  : true,
+        dataIndex : "url",
+        stateId   : "url",
+        renderer  : function(value) {
+            return "<a href=\"" + value + "\" target=\"_blank\">" + value + "</a>";
+        }
+    }],
+
+    initComponent: function() {
+        var me = this;
+        Ext.apply(me, {
+            store : Ext.create("OMV.data.Store", {
+                autoLoad : true,
+                model    : OMV.data.Model.createImplicit({
+                    idProperty : "uuid",
+                    fields     : [
+                        { name : "uuid", type: "string" },
+                        { name : "name", type: "string" },
+                        { name : "fullname", type: "string" },
+                        { name : "url", type: "string" }
+                    ]
+                }),
+                proxy    : {
+                    type    : "rpc",
+                    rpcData : {
+                        service : "Developer",
+                        method  : "getPluginList"
+                    }
+                }
+            })
+        });
+        me.callParent(arguments);
+    },
+
+    getTopToolbarItems: function() {
+        var me = this;
+        var items = me.callParent(arguments);
+
+        Ext.Array.insert(items, 0, [{
+            id       : me.getId() + "-updateall",
+            xtype    : "button",
+            text     : _("Update All"),
+            icon     : "images/refresh.png",
+            iconCls  : Ext.baseCSSPrefix + "btn-icon-16x16",
+            handler  : Ext.Function.bind(me.onUpdateButton, me, [ "all" ]),
+            scope    : me,
+            disabled : true
+        },{
+            id       : me.getId() + "-update",
+            xtype    : "button",
+            text     : _("Update"),
+            icon     : "images/refresh.png",
+            iconCls  : Ext.baseCSSPrefix + "btn-icon-16x16",
+            handler  : Ext.Function.bind(me.onUpdateButton, me, [ "" ]),
+            scope    : me,
+            disabled : true
+        },{
+            id       : me.getId() + "-build",
+            xtype    : "button",
+            text     : _("Build"),
+            icon     : "images/software.png",
+            iconCls  : Ext.baseCSSPrefix + "btn-icon-16x16",
+            handler  : Ext.Function.bind(me.onBuildButton, me, [ me ]),
+            scope    : me
+        }]);
+        return items;
+    },
+
+    onSelectionChange: function(model, records) {
+        var me = this;
+        me.callParent(arguments);
+        // Process additional buttons.
+        var tbarBtnDisabled = {
+            "update" : true,
+            "build"  : true
+        };
+        if(records.length == 1) {
+            tbarBtnDisabled["update"] = false;
+            tbarBtnDisabled["build"] = false;
+        }
+        // Update the button controls.
+        Ext.Object.each(tbarBtnDisabled, function(key, value) {
+            this.setToolbarButtonDisabled(key, value);
+        }, me);
+    },
+
+    onUpdateButton : function(plugin) {
+        var me = this;
+        var title = "";
+        var cmd = "";
+        if(plugin == "all") {
+            title = _("Updating all plugins...");
+            cmd = "all";
+        } else {
+            var record = me.getSelected();
+            name = record.get("fullname");
+            title = _("Updating ") + name + "...";
+            cmd = record.get("name");
+        }
+        var wnd = Ext.create("OMV.window.Execute", {
+            title           : title,
+            rpcService      : "Developer",
+            rpcMethod       : "doCommand",
+            rpcParams       : {
+                build   : false,
+                command : cmd
+            },
+            rpcIgnoreErrors : true,
+            hideStartButton : true,
+            hideStopButton  : true,
+            listeners       : {
+                scope     : me,
+                finish    : function(wnd, response) {
+                    wnd.appendValue(_("Done..."));
+                    wnd.setButtonDisabled("close", false);
+                },
+                exception : function(wnd, error) {
+                    OMV.MessageBox.error(null, error);
+                    wnd.setButtonDisabled("close", false);
+                }
+            }
+        });
+        wnd.setButtonDisabled("close", true);
+        wnd.show();
+        wnd.start();
+    },
+
+    onBuildButton : function() {
+        var me = this;
+        var record = me.getSelected();
+        var wnd = Ext.create("OMV.window.Execute", {
+            title           : title,
+            rpcService      : "Developer",
+            rpcMethod       : "doCommand",
+            rpcParams       : {
+                build   : true,
+                command : record.get("name")
+            },
+            rpcIgnoreErrors : true,
+            hideStartButton : true,
+            hideStopButton  : true,
+            listeners       : {
+                scope     : me,
+                finish    : function(wnd, response) {
+                    wnd.appendValue(_("Done..."));
+                    wnd.setButtonDisabled("close", false);
+                },
+                exception : function(wnd, error) {
+                    OMV.MessageBox.error(null, error);
+                    wnd.setButtonDisabled("close", false);
+                }
+            }
+        });
+        wnd.setButtonDisabled("close", true);
+        wnd.show();
+        wnd.start();
+    }
+});
+
+OMV.WorkspaceManager.registerPanel({
+    id        : "plugins",
+    path      : "/service/developer",
+    text      : _("Plugins"),
+    position  : 20,
+    className : "OMV.module.admin.service.developer.Plugins"
+});
---
+++ var/www/openmediavault/js/omv/module/admin/service/developer/Settings.js
@@ -0,0 +1,65 @@
+/**
+ * @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+ * @author    Volker Theile <volker.theile@openmediavault.org>
+ * @author    OpenMediaVault Plugin Developers <plugins@omv-extras.org>
+ * @copyright Copyright (c) 2009-2013 Volker Theile
+ * @copyright Copyright (c) 2014 OpenMediaVault Plugin Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+// require("js/omv/WorkspaceManager.js")
+// require("js/omv/workspace/form/Panel.js")
+// require("js/omv/workspace/window/Form.js")
+// require("js/omv/data/Store.js")
+// require("js/omv/data/Model.js")
+// require("js/omv/data/proxy/Rpc.js")
+// require("js/omv/workspace/window/plugin/ConfigObject.js")
+
+Ext.define("OMV.module.admin.service.devloper.Settings", {
+    extend : "OMV.workspace.form.Panel",
+    uses   : [
+        "OMV.Rpc",
+        "OMV.data.Store",
+        "OMV.data.Model",
+        "OMV.data.proxy.Rpc"
+    ],
+
+    rpcService   : "Developer",
+    rpcGetMethod : "getSettings",
+    rpcSetMethod : "setSettings",
+
+    getFormItems    : function() {
+        var me = this;
+        return [{
+            xtype    : "fieldset",
+            title    : _("Settings"),
+            defaults : {
+                labelSeparator : ""
+            },
+            items : [{
+                xtype      : "sharedfoldercombo",
+                name       : "sharedfolderref",
+                fieldLabel : _("Directory")
+            }]
+        }];
+    }
+});
+
+OMV.WorkspaceManager.registerPanel({
+    id        : "settings",
+    path      : "/service/developer",
+    text      : _("Settings"),
+    position  : 10,
+    className : "OMV.module.admin.service.developer.Settings"
+});
